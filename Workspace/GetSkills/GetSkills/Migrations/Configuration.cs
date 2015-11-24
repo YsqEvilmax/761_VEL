88f248e54c999f798a31dacf2592ca9f881e1249
YsqEvilmax <ysq.evilmax@qq.com>
2015-11-24 22:23:58 +1300

    1.deploy the database to a remote server 2.Add default admin account

--- Workspace/GetSkills/GetSkills/Migrations/Configuration.cs
+++ Workspace/GetSkills/GetSkills/Migrations/Configuration.cs
@@ -30,14 +30,26 @@ namespace GetSkills.Migrations
             //    );
             //
 
+            AddAdminAccount(context, "ysq.evilmax@qq.com", "ai3563623", "Admin");
+            AddAdminAccount(context, "getskills@outlook.com", "yogi1234", "Admin");
+        }
+
+        public void AddAdminAccount(GetSkills.Models.ApplicationDbContext context, string email, string psw, string roleName)
+        {
             var rm = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
             var um = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
 
-            rm.Create(new IdentityRole("Admin"));
+            if (!rm.RoleExists(roleName))
+            {
+                rm.Create(new IdentityRole(roleName));
+            }
 
-            var user = new ApplicationUser { UserName = "ysq.evilmax@qq.com", Email = "ysq.evilmax@qq.com" };
-            um.Create(user, "ai3563623");
-            um.AddToRole(user.Id, "Admin");
+            if (um.Find(email, psw) == null)
+            {
+                var user = new ApplicationUser { UserName = email, Email = email };
+                um.Create(user, psw);
+                um.AddToRole(user.Id, roleName);
+            }
         }
     }
 }
